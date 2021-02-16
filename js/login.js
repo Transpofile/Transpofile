b1f265accde398d98027b3e155858d6e4df08267
Transpofile <79129356+Transpofile@users.noreply.github.com>
2021-02-16 14:08:20 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "Mastergilbert" && password == "Mastergilbert26"){
+if ( username == "GA-transpofile" && password == "transpofile2021"){
 alert ("Login successfully");
 window.location = "html/Home.html"; // Redirecting to other page.
 return false;
