f3a923219dedeb67127aa817d1d68dd914f428ae
Transpofile <79129356+Transpofile@users.noreply.github.com>
2021-03-01 12:19:31 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,12 +5,12 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "GA-transpofile" && password == "transpofile2021"){
 alert ("Login successfully");
-window.location = "html/Home.html"; // Redirecting to other page.
+window.location = "html/qrcode.html"; // Redirecting to other page.
 return false;
 }
 else{
 attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
+alert("You have left "+attempt+" attempt; Authorize Personnel to Login only");
 // Disabling fields after 3 attempts.
 if( attempt == 0){
 document.getElementById("username").disabled = true;
